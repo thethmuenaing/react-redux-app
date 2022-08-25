bf01365884e4e6f388063198c9916c39fb0a2891
thethmuenaing <thethmuenaing22112002@gmail.com>
2022-08-25 09:38:14 +0630

    adding the input type to NewCakeContainer.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import store from "./redux/store";
 import CakeContainer from "./components/CakeContainer";
 import HookCakeContainer from "./components/HookCakeContainer";
 import IceCreamContainer from "./components/iceCreamContainer";
+import NewCakeContainer from "./components/NewCakeContainer";
 
 function App() {
 	return (
@@ -12,6 +13,7 @@ function App() {
 				<HookCakeContainer />
 				<CakeContainer />
 				<IceCreamContainer />
+				<NewCakeContainer />
 			</div>
 		</Provider>
 	);
