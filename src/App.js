b11655e6703d762c220dd9f2595d28df775a6034
thethmuenaing <thethmuenaing22112002@gmail.com>
2022-08-26 23:56:15 +0630

    adding the redux-thunk and some code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import "./App.css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import CakeContainer from "./components/CakeContainer";
-import HookCakeContainer from "./components/HookCakeContainer";
-import IceCreamContainer from "./components/iceCreamContainer";
-import NewCakeContainer from "./components/NewCakeContainer";
-import ItemContainer from "./components/ItemContainer";
+import UserContainer from "./components/UserContainer";
 
 function App() {
 	return (
 		<Provider store={store}>
 			<div className="App">
-				<ItemContainer cake />
-				<ItemContainer />
-				<HookCakeContainer />
-				<CakeContainer />
-				<IceCreamContainer />
-				<NewCakeContainer />
+				<UserContainer />
 			</div>
 		</Provider>
 	);
